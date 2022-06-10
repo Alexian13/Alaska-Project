06e9237987bdf888d1bfd9a0a0649af9416eac0f
Alexander Reski Rantepadang <46733872+Alexian13@users.noreply.github.com>
2022-06-10 13:11:47 +0800

    push tble

--- mazer-ratatotok/application/views/Toko/toko_view.php
+++ mazer-ratatotok/application/views/Toko/toko_view.php
@@ -1,31 +1,111 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Halaman Utama</title>
-</head>
-<body>
-
-  <h1>daftar nama</h1>
-
-  <table>
-      <tr>
-        <td>no</td>
-        <td>nama</td>
-        <td>alama</td>
-        <td>pekerjaan</td>
-        <td>barang</td>
-      </tr>
-
-      <tr>
-        <td>1</td>
-        <td>oktavian</td>
-        <td>ratahan</td>
-        <td><a href="">Edit</a> | <a href="">Delete</a></td>
-      </tr>
-  </table>
-  
-</body>
-</html>
+<div class="col-12 grid-margin stretch-card">
+  <div class="card">
+    <div class="card-body">
+      <h4 class="card-title">Basic form elements</h4>
+      <p class="card-description"> Basic form elements </p>
+      <form class="forms-sample">
+        <div class="form-group">
+          <label for="exampleInputName1">Name</label>
+          <input type="text" class="form-control" id="exampleInputName1" placeholder="Name">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputEmail3">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail3" placeholder="Email">
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword4">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword4" placeholder="Password">
+        </div>
+        <div class="form-group">
+          <label for="exampleSelectGender">Gender</label>
+          <select class="form-control" id="exampleSelectGender">
+            <option>Male</option>
+            <option>Female</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <label>File upload</label>
+          <input type="file" name="img[]" class="file-upload-default">
+          <div class="input-group col-xs-12">
+            <input type="text" class="form-control file-upload-info" disabled="" placeholder="Upload Image">
+            <span class="input-group-append">
+              <button class="file-upload-browse btn btn-primary" type="button">Upload</button>
+            </span>
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="exampleInputCity1">City</label>
+          <input type="text" class="form-control" id="exampleInputCity1" placeholder="Location">
+        </div>
+        <div class="form-group">
+          <label for="exampleTextarea1">Textarea</label>
+          <textarea class="form-control" id="exampleTextarea1" rows="4"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary mr-2">Submit</button>
+        <button class="btn btn-dark">Cancel</button>
+      </form>
+    </div>
+    
+    <div class="card-body">
+      <h4 class="card-title">Inverse table</h4>
+      <p class="card-description"> Add class <code>.table-dark</code>
+      </p>
+      <div class="table-responsive">
+        <table class="table table-dark">
+          <thead>
+            <tr>
+              <th> # </th>
+              <th> First name </th>
+              <th> Amount </th>
+              <th> Deadline </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td> 1 </td>
+              <td> Herman Beck </td>
+              <td> $ 77.99 </td>
+              <td> May 15, 2015 </td>
+            </tr>
+            <tr>
+              <td> 2 </td>
+              <td> Messsy Adam </td>
+              <td> $245.30 </td>
+              <td> July 1, 2015 </td>
+            </tr>
+            <tr>
+              <td> 3 </td>
+              <td> John Richards </td>
+              <td> $138.00 </td>
+              <td> Apr 12, 2015 </td>
+            </tr>
+            <tr>
+              <td> 4 </td>
+              <td> Peter Meggik </td>
+              <td> $ 77.99 </td>
+              <td> May 15, 2015 </td>
+            </tr>
+            <tr>
+              <td> 5 </td>
+              <td> Edward </td>
+              <td> $ 160.25 </td>
+              <td> May 03, 2015 </td>
+            </tr>
+            <tr>
+              <td> 6 </td>
+              <td> John Doe </td>
+              <td> $ 123.21 </td>
+              <td> April 05, 2015 </td>
+            </tr>
+            <tr>
+              <td> 7 </td>
+              <td> Henry Tom </td>
+              <td> $ 150.00 </td>
+              <td> June 16, 2015 </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
